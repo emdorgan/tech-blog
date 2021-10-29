47f3f1d51ab1948c02fe50d4a978fa2f815241d8
Emily Dorgan <wmdorgan@gmail.com>
2021-10-29 14:48:21 -0700

    set up the connection to handlebars so that it would actually work, borrowed handlebars code from a previous assignment to understand how it worked

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,3 +3,5 @@ const router = require('express').Router();
 router.get('/', async (req, res) =>{
     res.render('homepage')
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
 
 const sess = {
   secret: 'Super secret secret',
@@ -17,6 +20,9 @@ const sess = {
     db: sequelize
   })
 };
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(session(sess));
 
