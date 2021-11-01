521fe90e00f53647fb2aeee0a8987f42c27138e8
Emily Dorgan <wmdorgan@gmail.com>
2021-11-01 16:12:17 -0700

    users now have the ability to create new posts which get entered into the db and displayed to the users

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,6 +2,21 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+//endpoint for /api/posts
 
+// POST request to add a new blogpost
+router.post('/', withAuth, async (req, res) => {
+    try {
+        console.log('i got here');
+        console.log(req.body);
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+      }
+});
 
 module.exports = router;
--- public/js/newpost.js
+++ public/js/newpost.js
@@ -1,5 +1,22 @@
 const newPostHandler = async (event) => {
     event.preventDefault();
 
-    // const name = 
+    const title = document.querySelector('#blog-title').value.trim();
+    const post_text = document.querySelector('#blog-content').value.trim();
+    if(title && post_text) {
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({title, post_text}),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to make blogpost');
+        }
+    }
 }
+
+document.querySelector('.new-post-form').addEventListener('submit', newPostHandler);
