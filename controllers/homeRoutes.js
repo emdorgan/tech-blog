b67fac027079d1a2eb9c768600fe9ccadcc82f4c
Emily Dorgan <wmdorgan@gmail.com>
2021-11-01 14:21:56 -0700

    got the dashboard set up so it redirects to login page for unauthenticated users, and displays a welcome message and all blogposts along with a new post sign

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,10 +24,28 @@ router.get('/', async (req, res) =>{
     
 });
 
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: {exclude: ['password'] },
+            include: [{model: Post}],
+        });
+
+        const user = userData.get({ plain: true });
+        console.log(user);
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/profile');
+      res.redirect('/dashboard');
       return;
     }
   
