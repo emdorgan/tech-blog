56dfd60cc904d3a03077ae3c8fe85df4684d63ce
Emily Dorgan <wmdorgan@gmail.com>
2021-10-31 21:34:38 -0700

    fixed a typo in withAuth helper, added some templating data for blogposts and login page, added client-side javascript for login/logout function

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,8 +2,37 @@ const router = require('express').Router();
 const withAuth = require('../utils/auth');
 const { User, Post, Comment } = require('../models');
 
-router.get('/', async (req, res) =>{
-    res.render('homepage')
-})
+router.get('/', withAuth, async (req, res) =>{
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                }
+            ]
+        });
+
+    const posts = postData.map((post) => post.get({plain: true}));
+
+    res.render('homepage', {
+        posts,
+        logged_in: req.session.logged_in
+    });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    
+});
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router;
