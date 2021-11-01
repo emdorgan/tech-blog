e9cfa7ffe2185dd513d6b1ec6d3d4931fac288ac
Emily Dorgan <wmdorgan@gmail.com>
2021-10-31 19:52:28 -0700

    set up the file structure for the controller, connected the dependencies and exports

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
+const { User, Post, Comment } = require('../models');
 
 router.get('/', async (req, res) =>{
     res.render('homepage')
